41bab1097be5f9c37a18953377800eff095a278a
Andrew Costa <andrew.costa@broadcom.com>
2024-08-21 21:20:42 +0000

    ignore linter warnings for G115
    Signed-off-by: Ivan Protsiuk <ivan.protsiuk@broadcom.com>

--- src/pkg/otelcolclient/otelcolclient.go
+++ src/pkg/otelcolclient/otelcolclient.go
@@ -242,7 +242,7 @@ func (c *Client) writeCounter(e *loggregator_v2.Envelope) {
 						TimeUnixNano: uint64(e.GetTimestamp()),
 						Attributes:   atts,
 						Value: &metricspb.NumberDataPoint_AsInt{
-							AsInt: int64(e.GetCounter().GetTotal()),
+							AsInt: int64(e.GetCounter().GetTotal()), //#nosec G115
 						},
 					},
 				},
