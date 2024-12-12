7a5fab6598b16699b0715664bfec41b229491db1
nicklas dohrn <nicklas.dohrn@sap.com>
2024-12-12 17:11:49 +0100

    Fix https_batch deadlock due to golang timer changes
    It makes tests hopefully more robust
    
    It also replaces most sleeps with Consistently and Eventually.
    It makes the timings more forgiving.
    
    This should make it reliable on weak hardware.
    
    Add functional options pattern to allow test configuration

--- src/pkg/egress/syslog/https_batch.go
+++ src/pkg/egress/syslog/https_batch.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"log"
+	"sync"
 	"time"
 
 	"code.cloudfoundry.org/go-loggregator/v10/rpc/loggregator_v2"
@@ -11,14 +12,27 @@ import (
 	"code.cloudfoundry.org/loggregator-agent-release/src/pkg/egress"
 )
 
-const BATCHSIZE = 256 * 1024
-
 type HTTPSBatchWriter struct {
 	HTTPSWriter
-	msgs         chan []byte
 	batchSize    int
 	sendInterval time.Duration
-	egrMsgCount  float64
+	msgChan      chan []byte
+	quit         chan struct{}
+	wg           sync.WaitGroup
+}
+
+type Option func(*HTTPSBatchWriter)
+
+func WithBatchSize(size int) Option {
+	return func(w *HTTPSBatchWriter) {
+		w.batchSize = size
+	}
+}
+
+func WithSendInterval(interval time.Duration) Option {
+	return func(w *HTTPSBatchWriter) {
+		w.sendInterval = interval
+	}
 }
 
 func NewHTTPSBatchWriter(
@@ -27,10 +41,12 @@ func NewHTTPSBatchWriter(
 	tlsConf *tls.Config,
 	egressMetric metrics.Counter,
 	c *Converter,
+	options ...Option,
 ) egress.WriteCloser {
 	client := httpClient(netConf, tlsConf)
-	binding.URL.Scheme = "https" // reset the scheme for usage to a valid http scheme
-	BatchWriter := &HTTPSBatchWriter{
+	binding.URL.Scheme = "https"
+
+	writer := &HTTPSBatchWriter{
 		HTTPSWriter: HTTPSWriter{
 			url:             binding.URL,
 			appID:           binding.AppID,
@@ -39,60 +55,79 @@ func NewHTTPSBatchWriter(
 			egressMetric:    egressMetric,
 			syslogConverter: c,
 		},
-		batchSize:    BATCHSIZE,
-		sendInterval: 1 * time.Second,
-		egrMsgCount:  0,
-		msgs:         make(chan []byte),
+		batchSize:    256 * 1024,        // Default value
+		sendInterval: 1 * time.Second,   // Default value
+		msgChan:      make(chan []byte), // Buffered channel for messages
+		quit:         make(chan struct{}),
 	}
-	go BatchWriter.startSender()
-	return BatchWriter
+
+	for _, opt := range options {
+		opt(writer)
+	}
+
+	writer.wg.Add(1)
+	go writer.startSender()
+
+	return writer
 }
 
-// Modified Write function
 func (w *HTTPSBatchWriter) Write(env *loggregator_v2.Envelope) error {
 	msgs, err := w.syslogConverter.ToRFC5424(env, w.hostname)
 	if err != nil {
-		log.Printf("Failed to parse syslog, dropping faulty message, err: %s", err)
+		log.Printf("Failed to parse syslog, dropping message, err: %s", err)
 		return nil
 	}
 
 	for _, msg := range msgs {
-		//There is no correct way of implementing error based retries in the current architecture.
-		//Retries for https-batching will be implemented at a later point in time.
-		w.msgs <- msg
+		w.msgChan <- msg
 	}
+
 	return nil
 }
 
 func (w *HTTPSBatchWriter) startSender() {
-	t := time.NewTimer(w.sendInterval)
+	defer w.wg.Done()
+
+	ticker := time.NewTicker(w.sendInterval)
+	defer ticker.Stop()
 
 	var msgBatch bytes.Buffer
 	var msgCount float64
-	reset := func() {
-		msgBatch.Reset()
-		msgCount = 0
-		t.Reset(w.sendInterval)
-	}
-	for {
-		select {
-		case msg := <-w.msgs:
-			length, buffer_err := msgBatch.Write(msg)
-			if buffer_err != nil {
-				log.Printf("Failed to write to buffer, dropping buffer of size %d , err: %s", length, buffer_err)
-				reset()
-			} else {
-				msgCount++
-				if length >= w.batchSize {
-					w.sendHttpRequest(msgBatch.Bytes(), msgCount) //nolint:errcheck
-					reset()
-				}
-			}
-		case <-t.C:
-			if msgBatch.Len() > 0 {
-				w.sendHttpRequest(msgBatch.Bytes(), msgCount) //nolint:errcheck
-				reset()
-			}
+
+	sendBatch := func() {
+		if msgBatch.Len() > 0 {
+			w.sendHttpRequest(msgBatch.Bytes(), msgCount) // nolint:errcheck
+			msgBatch.Reset()
+			msgCount = 0
 		}
 	}
+
+	for {
+		select {
+		case msg := <-w.msgChan:
+			_, err := msgBatch.Write(msg)
+			if err != nil {
+				log.Printf("Failed to write to buffer, dropping buffer of size %d , err: %s", msgBatch.Len(), err)
+				msgBatch.Reset()
+				msgCount = 0
+			} else {
+				msgCount++
+				if msgBatch.Len() >= w.batchSize {
+					sendBatch()
+				}
+			}
+		case <-ticker.C:
+			sendBatch()
+		case <-w.quit:
+			sendBatch()
+			return
+		}
+	}
+}
+
+func (w *HTTPSBatchWriter) Close() error {
+	close(w.quit)
+	w.wg.Wait() // Ensure sender finishes processing before closing
+	close(w.msgChan)
+	return nil
 }
