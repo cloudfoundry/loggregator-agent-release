031148f78d9e01b2601f6fd5669a07fa4ea79766
Jovan Kostovski <jovan.kostovski@sap.com>
2026-08-05 15:59:18 +0200

    filtered_binding_fetcher.go - update filter param names

--- src/pkg/ingress/bindings/filtered_binding_fetcher.go
+++ src/pkg/ingress/bindings/filtered_binding_fetcher.go
@@ -72,7 +72,7 @@ func (f *FilteredBindingFetcher) FetchBindings() ([]syslog.Binding, error) {
 
 		if invalidLogFilter(u) {
 			invalidDrains += 1
-			f.printWarning("include-log-source-types and exclude-log-source-types cannot be used at the same time in syslog drain url %s for application %s", anonymousUrl.String(), b.AppId)
+			f.printWarning("include-log-types and exclude-log-types cannot be used at the same time in syslog drain url %s for application %s", anonymousUrl.String(), b.AppId)
 			continue
 		}
 
@@ -113,21 +113,21 @@ func (f *FilteredBindingFetcher) FetchBindings() ([]syslog.Binding, error) {
 
 }
 
-// invalidLogFilter checks if both include-log-source-types and exclude-log-source-types
+// invalidLogFilter checks if both include-log-types and exclude-log-types are set
 func invalidLogFilter(u *url.URL) bool {
-	includeSourceTypes := u.Query().Get("include-log-source-types")
-	excludeSourceTypes := u.Query().Get("exclude-log-source-types")
+	includeSourceTypes := u.Query().Get("include-log-types")
+	excludeSourceTypes := u.Query().Get("exclude-log-types")
 	if excludeSourceTypes != "" && includeSourceTypes != "" {
 		return true
 	}
 	return false
 }
 
-// assumes only one of include-log-source-types or exclude-log-source-types is set
+// assumes only one of include-log-types or exclude-log-types is set
 func getUnknownSourceTypes(u url.Values) []string {
 	var sourceTypeList string
-	includeSourceTypes := u.Get("include-log-source-types")
-	excludeSourceTypes := u.Get("exclude-log-source-types")
+	includeSourceTypes := u.Get("include-log-types")
+	excludeSourceTypes := u.Get("exclude-log-types")
 
 	if includeSourceTypes != "" {
 		sourceTypeList = includeSourceTypes
